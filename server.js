1cdc27831f7462ac8ae3046a662bccb5f90b3826
dalazaro <daryl.lazaro@gmail.com>
2017-07-20 11:41:27 -0700

    Step 3: reconnected script, styles, and images tags in index.html

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
+//Set up express app to serve public directory as static
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', function(req, res){
   res.sendFile('views/index.html' , { root : __dirname});
-  console.log(__dirname);
-})
+});
+
 
 app.listen(3000, function () {
   console.log('Express server is up and running on http://localhost:3000/');
